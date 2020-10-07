cef9bdfd7309268238e0d1aa02c46ecaae87ce65
Wenfei Zhou <wzhou2046@gmail.com>
2020-10-07 09:22:36 -0400

    added namespaces function

--- slack/public/scripts.js
+++ slack/public/scripts.js
@@ -1,30 +1,39 @@
-const username = prompt('What is your username?');
-// const socket = io('http://localhost:9000'); // the / namespace/endpoint
-const socket = io('http://localhost:9000', {
-  query: {
-    username,
-  },
-});
-let nsSocket = '';
-// listen for nsList, which is a list of all the namespaces.
-socket.on('nsList', (nsData) => {
-  console.log('The list of .rooms has arrived!!');
-  // console.log(nsData)
-  let namespacesDiv = document.querySelector('.namespaces');
-  namespacesDiv.innerHTML = '';
-  nsData.forEach((ns) => {
-    namespacesDiv.innerHTML += `<div class="namespace" ns=${ns.endpoint} ><img src="${ns.img}" /></div>`;
-  });
+// const username = prompt('What is your username?');
+const socket = io('http://localhost:9000/'); // the / namespace/endpoint
 
-  // Add a clicklistener for each NS
-  console.log(document.getElementsByClassName('namespace'));
-  Array.from(document.getElementsByClassName('namespace')).forEach((elem) => {
-    // console.log(elem)
-    elem.addEventListener('click', (e) => {
-      const nsEndpoint = elem.getAttribute('ns');
-      // console.log(`${nsEndpoint} I should go to now`)
-      joinNs(nsEndpoint);
-    });
-  });
-  joinNs('/wiki');
+socket.on('connect', () => {
+  console.log(socket.id);
 });
+
+socket.on('nsList', (nsData) => {
+  console.log('the list namespaces has arrived');
+  // console.log(nsData);
+});
+// const socket = io('http://localhost:9000', {
+//   query: {
+//     username,
+//   },
+// });
+// let nsSocket = '';
+// // listen for nsList, which is a list of all the namespaces.
+// socket.on('nsList', (nsData) => {
+//   console.log('The list of .rooms has arrived!!');
+//   // console.log(nsData)
+//   let namespacesDiv = document.querySelector('.namespaces');
+//   namespacesDiv.innerHTML = '';
+//   nsData.forEach((ns) => {
+//     namespacesDiv.innerHTML += `<div class="namespace" ns=${ns.endpoint} ><img src="${ns.img}" /></div>`;
+//   });
+
+//   // Add a clicklistener for each NS
+//   console.log(document.getElementsByClassName('namespace'));
+//   Array.from(document.getElementsByClassName('namespace')).forEach((elem) => {
+//     // console.log(elem)
+//     elem.addEventListener('click', (e) => {
+//       const nsEndpoint = elem.getAttribute('ns');
+//       // console.log(`${nsEndpoint} I should go to now`)
+//       joinNs(nsEndpoint);
+//     });
+//   });
+//   joinNs('/wiki');
+// });
