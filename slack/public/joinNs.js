653aaba18a3eaf10d07ff9eeb45059b77292e375
Wenfei Zhou <wzhou2046@gmail.com>
2020-10-09 12:59:30 -0400

    added joining room

---
+++ slack/public/joinNs.js
@@ -0,0 +1,33 @@
+function joinNs(endPoint) {
+  const nsSocket = io(`http://localhost:9000${endPoint}`);
+  nsSocket.on('nsRoomLoad', (nsRooms) => {
+    // console.log(nsRooms);
+    let roomList = document.querySelector('.room-list');
+    roomList.innerHTML = '';
+    nsRooms.forEach((room) => {
+      let glyph = room.privateRoom ? 'lock' : 'globe';
+
+      roomList.innerHTML += `<li class="room"><span class="glyphicon glyphicon-${glyph}"></span>${room.roomTitle}</li>`;
+    });
+
+    let roomNodes = document.getElementsByClassName('room');
+    Array.from(roomNodes).forEach((ele) => {
+      ele.addEventListener('click', (e) => {
+        console.log('someone clicked on', e.target.innerText);
+      });
+    });
+  });
+
+  nsSocket.on('messageToClients', (msg) => {
+    console.log(msg);
+    document.querySelector('#messages').innerHTML += `<li>${msg.text}</li>`;
+  });
+
+  document
+    .querySelector('.message-form')
+    .addEventListener('submit', (event) => {
+      event.preventDefault();
+      const newMessage = document.querySelector('#user-message').value;
+      socket.emit('newMessageToServer', { text: newMessage });
+    });
+}
--- slack/public/scripts.js
+++ slack/public/scripts.js
@@ -1,10 +1,6 @@
 // const username = prompt('What is your username?');
 const socket = io('http://localhost:9000/'); // the / namespace/endpoint
 
-socket.on('connect', () => {
-  console.log(socket.id);
-});
-
 socket.on('nsList', (nsData) => {
   console.log('the list namespaces has arrived');
   console.log(nsData);
@@ -21,47 +17,5 @@ socket.on('nsList', (nsData) => {
       console.log(nsEndpoint);
     });
   });
-  const nsSocket = io('http://localhost:9000/wiki');
-  nsSocket.on('nsRoomLoad', (nsRooms) => {
-    // console.log(nsRooms);
-    let roomList = document.querySelector('.room-list');
-    roomList.innerHTML = '';
-    nsRooms.forEach((room) => {
-      let glyph;
-      if (room.privateRoom) {
-        glyph = 'lock';
-      } else {
-        glyph = 'globe';
-      }
-      roomList.innerHTML += `<li"><span class="glyphicon glyphicon-${glyph}"></span>${room.roomTitle}</li>`;
-    });
-  });
+  joinNs('/wiki');
 });
-// const socket = io('http://localhost:9000', {
-//   query: {
-//     username,
-//   },
-// });
-// let nsSocket = '';
-// // listen for nsList, which is a list of all the namespaces.
-// socket.on('nsList', (nsData) => {
-//   console.log('The list of .rooms has arrived!!');
-//   // console.log(nsData)
-//   let namespacesDiv = document.querySelector('.namespaces');
-//   namespacesDiv.innerHTML = '';
-//   nsData.forEach((ns) => {
-//     namespacesDiv.innerHTML += `<div class="namespace" ns=${ns.endpoint} ><img src="${ns.img}" /></div>`;
-//   });
-
-//   // Add a clicklistener for each NS
-//   console.log(document.getElementsByClassName('namespace'));
-//   Array.from(document.getElementsByClassName('namespace')).forEach((elem) => {
-//     // console.log(elem)
-//     elem.addEventListener('click', (e) => {
-//       const nsEndpoint = elem.getAttribute('ns');
-//       // console.log(`${nsEndpoint} I should go to now`)
-//       joinNs(nsEndpoint);
-//     });
-//   });
-//   joinNs('/wiki');
-// });
