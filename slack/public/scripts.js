30bd240c2cf820622795d557c8163caaefbbd251
Wenfei Zhou <wzhou2046@gmail.com>
2020-10-11 11:36:32 -0400

    changing username

--- slack/public/scripts.js
+++ slack/public/scripts.js
@@ -1,5 +1,8 @@
-// const username = prompt('What is your username?');
-const socket = io('http://localhost:9000/'); // the / namespace/endpoint
+const socket = io('http://localhost:9000/', {
+  query: {
+    username: 'rbunch',
+  },
+}); // the / namespace/endpoint
 let nsSocket = '';
 
 socket.on('nsList', (nsData) => {
